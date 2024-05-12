cf9dfef475949a4a7f3e3d4ed38428b166a71407
Tapan Sharma <tapansharma69@gmail.com>
2024-05-12 13:25:10 +0545

    handling multiple error types with a single page

--- resources/js/Pages/Error.vue
+++ resources/js/Pages/Error.vue
@@ -1,13 +1,39 @@
 <script setup>
 import GuestLayout from "@/Layouts/GuestLayout.vue";
 import { Head } from "@inertiajs/vue3";
+import { computed } from "vue";
+
+const props = defineProps({
+    status: {
+        type: Number,
+        required: true,
+    },
+});
+
+const title = computed(() => {
+    return (
+        {
+            404: "Page Not Found",
+            403: "403 | Forbidden",
+        }[props.status] || "An Error Occurred"
+    );
+});
+
+const description = computed(() => {
+    return (
+        {
+            404: "The page you are looking for does not exist.",
+            403: "You are not authorized to access this page.",
+        }[props.status] || "An error occurred."
+    );
+});
 </script>
 
 <template>
-    <Head title="An error occured" />
+    <Head :title="title" />
     <GuestLayout>
         <div class="mb-4 font-medium text-sm text-green-600">
-            An Error Occurred
+            {{ description }}
         </div>
     </GuestLayout>
 </template>
