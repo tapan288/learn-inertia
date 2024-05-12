a8fb82c9e726d695fc2f8c792a49264ecd9c1571
Tapan Sharma <tapansharma69@gmail.com>
2024-05-12 12:48:25 +0545

    returning an inertia response

---
+++ resources/js/Pages/Error.vue
@@ -0,0 +1,13 @@
+<script setup>
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+import { Head } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <Head title="An error occured" />
+    <GuestLayout>
+        <div class="mb-4 font-medium text-sm text-green-600">
+            An Error Occurred
+        </div>
+    </GuestLayout>
+</template>
